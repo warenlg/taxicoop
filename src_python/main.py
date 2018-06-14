00947172b6cbf174c02c99a76da0faa2079ffa4c
Waren Long <waren@sourced.tech>
2018-06-14 18:36:12 +0200

    Filter corrupted requests
    Signed-off-by: Waren Long <waren@sourced.tech>

--- src_python/main.py
+++ src_python/main.py
@@ -3,6 +3,7 @@ import csv
 import logging
 import os
 import sys
+from typing import Tuple, List
 
 import numpy
 
@@ -13,12 +14,20 @@ def setup():
                         help="Path to the dataset of taxi requests.")
     parser.add_argument("-s", "--size", type=int, default=None,
                         help="Maximum size of the dataset to import.")
+    parser.add_argument("-v", "--speed", type=int, default=40,
+                        help="Constant speed for the taxis: 40km/h in Santos's paper.")
+    parser.add_argument("-c", "--capacity", type=int, default=2,
+                        help="Seat capacity of each taxi, driver not included.")
+    parser.add_argument("--alpha", type=float, default=1,
+                        help="Customer are paying less than an individual ride times alpha.")
+    parser.add_argument("--max-iter", type=int, default=100,
+                        help="Maximum number of iterations in the algorithm.")
     logging.basicConfig(level=logging.INFO)
     args = parser.parse_args()
     return args
 
 
-def read_dataset(path: str, size: int=None) -> numpy.ndarray:
+def read_dataset(path: str, size: int=None) -> List:
     """
     Function to read the input CSV file with the taxi requests.
 
@@ -29,7 +38,7 @@ def read_dataset(path: str, size: int=None) -> numpy.ndarray:
     param: size is an upper bound for the number of rows in the CSV we are reading because
     those CSV files count millions of rows.
 
-    return: a numpy array of taxi requests where one row is
+    return: a list of taxi requests where one row is
     [PU_datetime, DO_datetime, PU_longitude, PU_latitude, DO_longitude, DO_latitude]
     """
     log = logging.getLogger("reader")
@@ -40,12 +49,13 @@ def read_dataset(path: str, size: int=None) -> numpy.ndarray:
         fin.seek(0)
         reader = csv.reader(fin)
         next(reader) # to not read the headers
-        requests = numpy.zeros((size, 6), dtype=numpy.float32)
+        dataset = []
         for i, row in enumerate(reader):
             if i % 1000 == 0:
                 sys.stderr.write("%d\r" % i)
             if i >= size:
                 break
+            request = []
             for j, val in enumerate(row):
                 if j in (1, 2):
                     datetime = val.split()
@@ -53,20 +63,24 @@ def read_dataset(path: str, size: int=None) -> numpy.ndarray:
                     timestamp = [int(x) for x in timestamp]
                     hour, minutes, seconds = tuple(timestamp)
                     time_absolute = hour*3600 + minutes*60 + seconds
-                    requests[i][j-1] = time_absolute
+                    request.append(time_absolute)
                 elif j in (5, 6):
-                    requests[i][j-3] = float(val)
+                    request.append(float(val))
                 elif j in (9, 10):
-                    requests[i][j-5] = float(val)
+                    request.append(float(val))
+            # some requests are corrupted, we don't add them to the dataset
+            if request[0] < request[1]:
+                dataset.append(request)
     finally:
         sys.stderr.write("\n")
         fin.close()
-    return requests
+    return dataset
 
 
 def main():
     args = setup()
-    requests = read_dataset(args.input, args.size)  
+    dataset = read_dataset(args.input, args.size)
+    print("len_dataset :", len(dataset)) 
 
 
 if __name__ == "__main__":
